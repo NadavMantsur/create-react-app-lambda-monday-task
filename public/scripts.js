adbbc7053c51a4bd2ab73c3eabba68c675ff36f6
linoy ganti <linoyganti@gmail.com>
2022-04-06 20:39:03 +0300

    starting the design

---
+++ public/scripts.js
@@ -0,0 +1,11 @@
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+
+startButton.addEventListener('click', startGame)
+
+function startGame(){
+    console.log('started')
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+}
+
