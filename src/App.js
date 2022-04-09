e68cb3dd6a79249e2963f409c826dba1ac98cd1e
LINOY <nadav40450@gmail.com>
2022-04-09 20:25:54 +0300

    final 1

--- src/App.js
+++ src/App.js
@@ -1,69 +1,76 @@
-import React, { useState } from 'react';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
-export default function App() {
+function App() {
+  const [data, setData] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [correctAnswersCounter, setCorrectAnswersCounter] = useState(0);
 
-	const [data, setData] = useState([]);
+  
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=100")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log("success");
+        setData(json.results);
+      });
+  }, []);
 
-	const [currentQuestion, setCurrentQuestion] = useState(0);
+  console.log(data);
 
-	const [showScore, setShowScore] = useState(false);
+  return (
+    <div
+      className="container"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100vw",
+        justifyContent: "center",
+      }}
+    > <div className="heading">Quiz Game</div><br></br>
+      <div className="question">{data[currentQuestion]?.question}</div><br></br>
 
-	const [score, setScore] = useState(0);
+      <div className="score">Score:{correctAnswersCounter}</div><br></br>
 
-	const[scoreList, setScoreList] = useState(0);
+      <div className="incorrectAnswers">
+        {data[currentQuestion]?.incorrect_answers?.map((answer) => {
+          return (
+            <div className="button">
+              <button
+                onClick={() => {
+                  setCurrentQuestion((prev) => prev + 1);
+                }}
+              >
+                {answer}
+              </button>
+            </div>
+          );
+        })}
+      </div>
 
-	const handleAnswerButton = (isCorrect) => {
-		if(isCorrect === true) {
-			setScore(score +1);
-		}
-
-		const nextQuestion = currentQuestion +1;
-		if(nextQuestion < data.length) {
-			setCurrentQuestion(nextQuestion);
-		}else{
-			setShowScore(true);
-		}
-	}
-
-	const handleResetButton = (score) => {
-		setScoreList(scoreList+score);
-		setCurrentQuestion(0);
-		setShowScore(false);
-		setScore(0);
-	}
-
-	React.useEffect(() => {
-		fetch("https://opentdb.com/api.php?amount=100")
-		  .then((response) => response.json())
-		  .then((json) => {
-			console.log("success");
-			setData(json.results);
-		  });
-	  }, []);
-	
-	  console.log(data);
-
-	return (
-		<div className='app'>
-			{showScore ? (
-				<div className='score-section'>You scored {score} out of {data.length} and your acummulated score is {scoreList}
-				<button onClick={() => handleResetButton(score)}>Play Again!</button></div>
-
-			) : (
-				<>
-					<div className='question-section'>
-						<div className='question-count'>
-							<span>Question {currentQuestion + 1}</span>/{data.length}
-						</div>
-						<div className='question-text'>{data[currentQuestion]?.question}</div>
-					</div>
-					<div className='answer-section'>
-						{data[currentQuestion].answerOptions.map((answerOption) => (
-							<button onClick={() => handleAnswerButton(answerOption.isCorrect)}>{answerOption.answerText}</button>
-						))}
-					</div>
-				</>
-			)}
-		</div>
-	);
+      <div className="incorrectAnswers">
+        {data[currentQuestion]?.correct_answers?.map((answer) => {
+          return (
+            <div className="button">
+              <button
+                onClick={() => {
+                  setCurrentQuestion((prev) => prev + 1);
+                }}
+              >
+                {answer}
+              </button>
+            </div>
+          );
+        })}
+            <div>
+              <button onClick={() => {
+                  setCurrentQuestion((prev) => prev + 1);
+                  setCorrectAnswersCounter(correctAnswersCounter + 1);
+                }}>{data[currentQuestion]?.correct_answer}</button>
+            </div>
+      </div>
+    </div>
+  );
 }
+
+export default App;
